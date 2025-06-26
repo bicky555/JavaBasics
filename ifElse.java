83a2619413a12b8ab622f400cf7b70511e09b0e6
Bicky jaiswal <bickyjaiswal90@gmail.com>
2025-06-26 15:33:26 +0530

    New Line

--- ifElse.java
+++ ifElse.java
@@ -1,2 +1,11 @@
 public class ifElse {
+    public static void main(String[] args) {
+        int i = 1;
+        if(i==0){
+            System.out.println("print "+i);
+        }
+        else {
+            System.out.println("Don't print");
+        }
+    }
 }
